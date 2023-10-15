b1abdeaf3fb44d162d04f43321a33670133c50e9
elizabethpurtell <epurtell.CF23@gmail.com>
2023-10-15 16:37:54 -0700

    added js game code, table code filled with localstorage to the home page, extra files added that are only for testing purposes and will be deleted when complete

--- js/home.js
+++ js/home.js
@@ -1,21 +1,34 @@
-// *** Global Variables ***
+'use strict';
 
+document.addEventListener('DOMContentLoaded', function () {
+  // Retrieve player data from localStorage
+  const playerData = JSON.parse(localStorage.getItem('playerData'));
 
+  if (playerData) {
+      const playerName = playerData.name;
+      const playerScore = playerData.score;
+      const rank = playerData.rank;
 
-// *** Constructor functions ***
+      // Append the player data to the table
+      const scoreTableBody = document.getElementById('scoreTableBody');
 
+      // Create a new table row
+      const newRow = document.createElement('tr');
 
+      // Create and populate table cells for player data
+      const playerNameCell = document.createElement('td');
+      playerNameCell.textContent = playerName;
+      const playerScoreCell = document.createElement('td');
+      playerScoreCell.textContent = playerScore;
+      const rankCell = document.createElement('td');
+      rankCell.textContent = rank;
 
-// *** Prototype Methods ***
+      // Append cells to the new row
+      newRow.appendChild(playerNameCell);
+      newRow.appendChild(playerScoreCell);
+      newRow.appendChild(rankCell);
 
-
-
-// *** Regular Functions ***
-
-
-
-// *** Event Listeners ***
-
-
-
-// *** Call Functions ***
+      // Append the row to the table body
+      scoreTableBody.appendChild(newRow);
+  }
+});
